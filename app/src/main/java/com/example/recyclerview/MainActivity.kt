a23283974be2def0f5e5d7abd77c9dcc18ae493f
Murad Ahmedov <code.exe.machine1337@gmail.com>
2022-06-09 13:06:20 +0300

    add popupMenu

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.recyclerview.databinding.ActivityMainBinding
+import com.example.recyclerview.model.User
 import com.example.recyclerview.model.UserListener
 import com.example.recyclerview.model.UserService
 
@@ -20,7 +22,19 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        adapter = UsersAdapter()
+        adapter = UsersAdapter(object : UserActionListener {
+            override fun onUserMove(user: User, moveBy: Int) {
+                usersService.moveUser(user, moveBy)
+            }
+
+            override fun onUserDelete(user: User) {
+                usersService.deleteUser(user)
+            }
+
+            override fun onUserDetails(user: User) {
+                Toast.makeText(this@MainActivity, "User: ${user.name}", Toast.LENGTH_SHORT).show()
+            }
+        })
 
         val layoutManager = LinearLayoutManager(this)
         binding.recyclerView.layoutManager = layoutManager
--- app/src/main/java/com/example/recyclerview/UsersAdapter.kt
+++ app/src/main/java/com/example/recyclerview/UsersAdapter.kt
@@ -1,13 +1,29 @@
 package com.example.recyclerview
 
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.PopupMenu
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.recyclerview.databinding.ItemUserBinding
 import com.example.recyclerview.model.User
 
-class UsersAdapter : RecyclerView.Adapter<UsersAdapter.UsersViewHolder>() {
+interface UserActionListener {
+
+    fun onUserMove(user: User, moveBy: Int)
+
+    fun onUserDelete(user: User)
+
+    fun onUserDetails(user: User)
+
+}
+
+
+class UsersAdapter(
+    private val actionListener: UserActionListener
+) : RecyclerView.Adapter<UsersAdapter.UsersViewHolder>(), View.OnClickListener {
 
     var users: List<User> = emptyList()
         set(newValue) {
@@ -18,22 +34,30 @@ class UsersAdapter : RecyclerView.Adapter<UsersAdapter.UsersViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UsersViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = ItemUserBinding.inflate(inflater, parent, false)
+
+        binding.root.setOnClickListener(this)
+        binding.ivMore.setOnClickListener(this)
+
         return UsersViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: UsersViewHolder, position: Int) {
         val user = users[position]
-        with(holder.binding){
+        with(holder.binding) {
+            holder.itemView.tag = user
+            ivMore.tag = user
+
             tvUserName.text = user.name
             tvUserCompany.text = user.company
-            if(user.photo.isNotBlank()){
+            if (user.photo.isNotBlank()) {
                 Glide.with(ivUserAvatar.context)
                     .load(user.photo)
                     .circleCrop()
                     .placeholder(R.drawable.ic_user_default_avatar)
                     .error(R.drawable.ic_user_error)
                     .into(ivUserAvatar)
-            }else{
+            } else {
+                Glide.with(ivUserAvatar.context).clear(ivUserAvatar)
                 ivUserAvatar.setImageResource(R.drawable.ic_user_default_avatar)
             }
         }
@@ -44,4 +68,54 @@ class UsersAdapter : RecyclerView.Adapter<UsersAdapter.UsersViewHolder>() {
     class UsersViewHolder(
         val binding: ItemUserBinding
     ) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onClick(p0: View) {
+        val user = p0.tag as User
+        when (p0.id) {
+            R.id.iv_more -> {
+                showPopupMenu(p0)
+            }
+            else -> {
+                actionListener.onUserDetails(user)
+            }
+        }
+    }
+
+    private fun showPopupMenu(view: View) {
+        val popupMenu = PopupMenu(view.context, view)
+        val context = view.context
+        val user = view.tag as User
+        val position = users.indexOfFirst { it.id == user.id }
+
+        popupMenu.menu.add(0, ID_MOVE_UP, Menu.NONE, context.getString(R.string.move_up)).apply {
+            isEnabled = position > 0
+        }
+        popupMenu.menu.add(0, ID_MOVE_DOWN, Menu.NONE, context.getString(R.string.move_down)).apply {
+            isEnabled = position < users.size - 1
+        }
+        popupMenu.menu.add(0, ID_MOVE_DELETE, Menu.NONE, context.getString(R.string.delete))
+
+        popupMenu.setOnMenuItemClickListener {
+            when (it.itemId) {
+                ID_MOVE_UP -> {
+                    actionListener.onUserMove(user, -1)
+                }
+                ID_MOVE_DOWN -> {
+                    actionListener.onUserMove(user, 1)
+                }
+                ID_MOVE_DELETE -> {
+                    actionListener.onUserDelete(user)
+                }
+            }
+            return@setOnMenuItemClickListener true
+        }
+
+        popupMenu.show()
+    }
+
+    companion object {
+        private const val ID_MOVE_UP = 1
+        private const val ID_MOVE_DOWN = 2
+        private const val ID_MOVE_DELETE = 3
+    }
 }
